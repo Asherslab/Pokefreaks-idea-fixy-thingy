bc7ccb24a77e28725eb73ac811264feae6be2b39
Unknown <Asher.parry@icloud.com>
2018-08-03 21:54:27 -0500

    testy mc test

--- src/main/java/com/idea/core/hooks/ChunkLoaderHook.java
+++ src/main/java/com/idea/core/hooks/ChunkLoaderHook.java
@@ -1,20 +1,12 @@
 package com.idea.core.hooks;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.crash.CrashReport;
-import net.minecraft.crash.CrashReportCategory;
-import net.minecraft.server.management.PlayerChunkMapEntry;
-import net.minecraft.util.ReportedException;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.ChunkProviderServer;
 import net.minecraft.world.gen.IChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
